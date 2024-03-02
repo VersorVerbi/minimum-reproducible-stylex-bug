b9933e945846693d59650cac1b4a26a2c005d5d8
VersorVerbi <doroapodeus@gmail.com>
2024-03-01 20:41:56 -0600

    feat: minimum reproducible ESLint bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import * as stylex from '@stylexjs/stylex';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const styles = stylex.create({
+  link: {
+    textDecoration: {
+      default: 'none',
+      ':hover': 'underline'
+    },
+    zIndex: 0
+  }
+});
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" {...stylex.props(styles.link)}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
